2369d13abc3dbc191a150f4d447ed401d83de76a
sesandi <sesandi.20220451@iit.ac.lk>
2023-11-15 21:43:03 +0530

    club class

--- src/main/java/com/example/sacms/Club.java
+++ src/main/java/com/example/sacms/Club.java
@@ -1,4 +1,51 @@
 package com.example.sacms;
 
 public class Club {
+
+    private String clubName;
+
+    private String clubDescription;
+
+    private String advisorName;
+
+    private String presidentName;
+
+    public Club (String clubDescription, String clubName, String advisorName, String presidentName){
+        this.clubName = clubName;
+        this.clubDescription = clubDescription;
+        this.advisorName = advisorName;
+        this.presidentName = presidentName;
+    }
+
+    public String getClubName() {
+        return clubName;
+    }
+
+    public void setClubName(String clubName) {
+        this.clubName = clubName;
+    }
+
+    public String getClubDescription() {
+        return clubDescription;
+    }
+
+    public void setClubDescription(String clubDescription) {
+        this.clubDescription = clubDescription;
+    }
+
+    public String getAdvisorName() {
+        return advisorName;
+    }
+
+    public void setAdvisorName(String advisorName) {
+        this.advisorName = advisorName;
+    }
+
+    public String getPresidentName() {
+        return presidentName;
+    }
+
+    public void setPresidentName(String presidentName) {
+        this.presidentName = presidentName;
+    }
 }
