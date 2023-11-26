8181f48dfdeb501d9b0f5f5fc2b20827a63b6ba6
RaviWella <ravindu.20220577@iit.ac.lk>
2023-11-26 23:39:13 +0530

    Combined three fxml to one fxml and retrieve advisor name

--- src/main/java/com/example/sacms/AdvisorController.java
+++ src/main/java/com/example/sacms/AdvisorController.java
@@ -6,10 +6,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.util.Objects;
@@ -47,8 +46,71 @@ public class AdvisorController {
     private TextField presidentNameInput;
 
 
+    @FXML
+    private AnchorPane advisorANC;
+
     private Club selectedClubRecord;
 
+    @FXML
+    private Button btnManageClubs;
+
+    @FXML
+    private Button btnAttendance;
+
+    @FXML
+    private Button btnSearch;
+
+    @FXML
+    private SplitPane manageClub;
+
+    @FXML
+    private AnchorPane clubAttendance;
+
+    @FXML
+    private AnchorPane attendanceTracking;
+
+    @FXML
+    private Label advisorName;
+
+
+    private final Database database = new Database();
+    private String username;
+
+
+    public void switchingPages(ActionEvent event){
+
+        if(event.getSource() == btnManageClubs ){
+            manageClub.setVisible(true);
+            clubAttendance.setVisible(false);
+            attendanceTracking.setVisible(false);
+
+
+        }else if(event.getSource() == btnAttendance){
+            manageClub.setVisible(false);
+            clubAttendance.setVisible(true);
+            attendanceTracking.setVisible(false);
+
+            Advisor advisor = database.getAdvisorData(username);
+
+            if(advisor != null){
+                setAdvisorData(advisor);
+            }
+
+        }else if(event.getSource() == btnSearch){
+            manageClub.setVisible(false);
+            clubAttendance.setVisible(false);
+            attendanceTracking.setVisible(true);
+        }
+    }
+
+
+    public void setUsername(String username){
+        this.username=username;
+    }
+
+    public void setAdvisorData(Advisor advisor){
+        advisorName.setText(advisor.getFirstName()+" "+advisor.getLastName());
+    }
 
     @FXML
     public void goBack(ActionEvent actionEvent)throws Exception {
@@ -57,16 +119,6 @@ public class AdvisorController {
         MainStage.setScene(new Scene(root));
         MainStage.setTitle("SACMS");
     }
-
-    public void onAttendance(ActionEvent actionEvent)throws Exception {
-
-        Stage MainStage =(Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("AdvisorClub.fxml")));
-        MainStage.setScene(new Scene(root));
-        MainStage.setTitle("SACMS");
-
-    }
-
 
     public void createClub(ActionEvent actionEvent) {
     }
